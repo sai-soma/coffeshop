10937345629d248ff5817cc7f260545a1d0d2b82
app1 <saieesh.soma1919@gmail.com>
2024-03-18 10:16:21 +0530

    Click Image

--- src/components/product/Products.js
+++ src/components/product/Products.js
@@ -1,21 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../../context/appContext.js";
 import products from "./products.json"
 import "./Product.css"
 import AddButton from "./AddButton.js";
+import ShowProduct from "./ShowProduct.js";
 export default function Products() {
-  const { cartItems, setCartItems } = useContext(AppContext);
+  const { cartItems, setCartItems, flag, setFlag } = useContext(AppContext);
   const PATH = process.env.REACT_APP_PATH
   // const addtoCart = (id) => {
   //   setCartItems((prev) => ({ ...prev, [id]: (prev[id] ?? 0) + 1 }));
   // };
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [showProducts, setShowProducts] = useState(false);
+  
+  const handleClick=(value)=>{
+    setShowProducts(!showProducts);
+    setSelectedProduct(value);
+  }
 
   return (
     <div className="Product-container">
-      {products &&
+      {products && !showProducts &&
         products.map((value) => (
           <div key={value.id} className="Product-items">
-            <div><img className="Product-img" src={`${PATH}${value.image}`} alt={value.name}/></div>
+            <div onClick={()=> handleClick(value)}><img className="Product-img" src={`${PATH}${value.image}`} alt={value.name} /></div>
             <h3>{value.name}</h3>
             <p style={{textAlign:"justify",padding:"10px"}}>{value.desc}</p>
             <div className="priceBtn">
@@ -25,6 +33,7 @@ export default function Products() {
             </div>
           </div>
         ))}
+        {selectedProduct && <ShowProduct product={selectedProduct}/>}
     </div>
   );
 }
