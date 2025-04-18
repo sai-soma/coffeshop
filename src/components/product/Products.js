fdb616eb50febd470214446cea32f4bcce01027e
app1 <saieesh.soma1919@gmail.com>
2025-04-18 13:53:24 +0530

    Images fix

--- src/components/product/Products.js
+++ src/components/product/Products.js
@@ -23,7 +23,7 @@ export default function Products() {
       {products && !showProducts &&
         products.map((value) => (
           <div key={value.id} className="Product-items">
-            <div onClick={()=> handleClick(value)}><img className="Product-img" src={`${PATH}${value.image}`} alt={value.name} /></div>
+            <div onClick={()=> handleClick(value)}><img className="Product-img" src={value.image} alt={value.name} /></div>
             <h3>{value.name}</h3>
             <p style={{textAlign:"justify",padding:"10px"}}>{value.desc}</p>
             <div className="priceBtn">
--- src/components/product/ShowProduct.js
+++ src/components/product/ShowProduct.js
@@ -2,11 +2,10 @@ import React from 'react'
 import AddButton from './AddButton'
 import Navbar from '../navbar/Navbar'
 export default function ShowProduct({product}) {
-    const PATH = process.env.REACT_APP_PATH
 
   return (
    <>
-   <img src={`${PATH}${product.image}`} width={250} height={250}/>
+   <img src={product.image} width={250} height={250}/>
    <p><b>{`${product.name}`}</b><br/>{`${product.desc}`} <br/><br/>
    <div className="Product-button"><AddButton id={product.id}/></div>
    </p>
